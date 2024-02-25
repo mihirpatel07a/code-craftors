ca496bb78b18051b71d97f8edf7db903ead0cbb7
prekshahirani <prekshahirani19@gmail.com>
2024-02-25 14:10:24 +0530

    Added thank you page

---
+++ frontend/src/components/pages/ThankYou.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Container, Row, Col, Button} from "reactstrap"
+import {Link} from "react-router-dom";
+import '../styles/thank-you.css'
+
+const ThankYou = () => {
+  return <section>
+    
+  </section>
+}
+
+export default ThankYou
