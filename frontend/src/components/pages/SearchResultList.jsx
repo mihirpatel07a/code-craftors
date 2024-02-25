9978575b005ebe3caeb6a879cf31637a25ea0e10
prekshahirani <prekshahirani19@gmail.com>
2024-02-25 12:07:48 +0530

    Added booking page

---
+++ frontend/src/components/pages/SearchResultList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchResultList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SearchResultList
--- frontend/src/components/router/Routers.js
+++ frontend/src/components/router/Routers.js
@@ -5,7 +5,9 @@ import Login from './../pages/Login';
 import SignUp from './../pages/Register';
 import Settings from '../pages/setting';
 import Home from "../pages/Home";
-
+import Tours from './../pages/Tours';
+import TourDetails from './../pages/TourDetails';
+import SearchResultList from '../pages/SearchResultList';
 const Routers = () => {
   return (
     <Routes>
@@ -15,6 +17,10 @@ const Routers = () => {
         {/* <Route path='/register' element={<Signup/>} /> */}
         <Route path='/register' element={<SignUp/>}/>
         <Route path='/settings' element={<Settings/>} />
+        <Route path='/tours' element={<Tours/>} />
+        <Route path='/tours/:id' element={<TourDetails/>} />
+        <Route path='/tours/search' element={<SearchResultList/>} />
+
     </Routes>
   )
 }
